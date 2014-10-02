72918559d7ab4fad538f28d5b0b166e3f990e37e
Russell Hampton <russellhampton@gmail.com>
2014-10-02 15:56:16 -0400

    Checking in for the day, WIP,added Opportunities tab

--- js/controllers.js
+++ js/controllers.js
@@ -1,12 +1,18 @@
-angular.module('starter.controllers', [])
+angular.module('pol.controllers', [])
 
 .controller('DashCtrl', function($scope) {
+})
+
+.controller('PhotoopsCtrl', function($scope, Photoops){
+	$scope.photoops=Photoops.all();
+})
+.controller('PhotoopDetailCtrl', function($scope, $stateParams, Photoops){
+	$scope.photoop=Photoops.get($stateParams.photoopId);
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
   $scope.friends = Friends.all();
 })
-
 .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
   $scope.friend = Friends.get($stateParams.friendId);
 })
